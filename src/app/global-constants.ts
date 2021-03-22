64183f193e9ae3e57596e74735473ae71ffd3b5a
locustbyte <locustbyte@me.com>
2021-03-22 09:56:32 +0000

    up

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -6,7 +6,7 @@ import { environment } from "../environments/environment";
 @Injectable()
 export class GlobalConstants {
     public REST_API_IP = "192.168.1.215";
-    public REST_API_SERVER = "https://" + this.REST_API_IP + ":5000/api/system";
+    public REST_API_SERVER = "http://" + this.REST_API_IP + ":5000/api/system";
     public APPS_AVAILABLE_SINGULAR = [];
     public APPS_AVAILABLE_MULTIPLE = [];
     public errorActive = false;
